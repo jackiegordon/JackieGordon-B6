bc4f0c06b8398f714afc1946ba30a74084d4fb62
jackietorres <jackietorres@jacquelines-air.fios-router.home>
2022-01-07 18:34:52 -0500

    Homework 12

---
+++ src/test/java/Homework12/MethodFinalHomework11Q1.java
@@ -0,0 +1,26 @@
+package Homework12;
+
+public class MethodFinalHomework11Q1 {
+
+
+public static void main(String[] args) {
+	
+	posNeg(1,-1,false);
+	System.out.println("************");
+	posNeg (-1,1,false);
+	System.out.println("************");
+	posNeg (-4,-5,false);
+	System.out.println("************");
+	
+}
+
+public static void posNeg (int numOne 2, int numTwo, boolean numThree) {
+
+	for (int i=-1 ; i<=1; i++) {
+		System.out.println("***********");
+		
+		return result;
+		
+	}
+}
+
---
+++ src/test/java/Homework12/MethodFinalHomework11Q2.java
@@ -0,0 +1,35 @@
+package Homework12;
+
+public class MethodFinalHomework11Q2 {
+
+}
+
+	multipleOf(3,true);
+	System.out.println("********");
+
+	multipleOf (10, true);
+	System.out.println("********");
+
+	multipleOf(8,false);
+	System.out.println("********");
+
+}
+	
+	public static void multipleOf (boolean modOne, boolean modTwo) {
+		
+		boolean result = ("boolean modOne, boolean modTwo");
+				
+				
+	} if (modOne == false) {
+		boolean result = true;
+	
+	}else if (modOne == true) {
+		boolean result = false;
+		
+	}else if (modtwo == false) {
+		boolean result = true;
+		
+		
+	}
+}
+}
