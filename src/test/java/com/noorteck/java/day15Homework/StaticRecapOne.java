6ec187fc0fb4302b39e5bf337a5b6a7072e920c7
jackietorres <jackietorres@jacquelines-air.fios-router.home>
2022-01-13 19:44:15 -0500

    Homework15

---
+++ src/test/java/com/noorteck/java/day15Homework/StaticRecapOne.java
@@ -0,0 +1,66 @@
+package com.noorteck.java.day15Homework;
+
+public class StaticRecapOne {
+
+	
+	public static boolean isJavaFun;
+	String month;
+	
+	public static int totalAmount;
+	String iLikeJava;
+	
+	String country;
+	int age;
+	char gender;
+	public static double temperature;
+
+	public void setCountry() {
+
+		//TO DO call [country] variable and assign value = "USA";
+		StaticRecapOne.country =  USA;
+	
+		//TO DO  print the value of variable [country]
+		System.out.println(StaticRecapOne.country);
+	}
+
+	public void getCountry() {
+
+		// call method [setCountry()]
+			setCountry();
+		// call variable [gender] and assign value 'f'
+		public static char grade ('f');
+		
+		
+		// print the [gender] variable
+		System.out.println(gender);
+		
+		// call variable[age] and assign value 11
+		getCountry obj = new getCountry
+				
+		// print variable [age]
+		public static int age;
+
+	}
+
+	public static void addNum(int a, int b) {
+		int result = a+b;	
+		System.out.println(result);
+		
+		
+		//TODO call [temperature] variable and assign value = 55.55;
+		StaticRecapOne.temperature = 55.55;
+		
+		//TODO  print the value of variable [temperature]
+		public static double temperature;
+
+		
+		 
+	}
+
+	public static void divNum(int a, int b) {
+
+
+		//TODO  call method[addNum()]
+		System.out.println(a / b);
+	}
+}
---
+++ src/test/java/com/noorteck/java/day15Homework/StaticRecapTwo.java
@@ -0,0 +1,45 @@
+package com.noorteck.java.day15Homework;
+
+public class StaticRecapTwo {
+
+	// static method
+		public static void getInfo() {
+
+			//TODO call [isJavaFun] variable and assign value = true
+				StaticRecapOne.isJavaFun=true;
+			//TODO call [totalAmount] variable and assign value = 1111
+				StaticRecapOne.totalAmount =1111;
+			//TODO print value of variable [isJavaFun]
+				System.out.println[StaticRecapOne.isJavaFun];		
+//TODO print value of variable [totalAmount]
+				System.out.println [StaticRecapOne.totalAmount];
+			//TODO call method [addNum()]
+				StaticRecapOne.addNum(2, 4);	 
+
+		}
+
+		// non-static method
+		public void statusUpdate() {
+
+			//TODO call [month] variable and assign value = "January"
+					obj.month ="January";
+
+			//TODO call [iLikeJava] variable and assign value = "Java is life"
+					obj.ilikeJava ="Java is life";
+			//TODO print value of variable [month]
+					System.out.println("obj.month");
+			//TODO print value of variable [iLikeJava]
+					System.out.println("obj.iLikeJava");
+
+			//TODO call method [getCountry()]
+					obj.getCountry();
+
+
+		}
+
+		public static void main(String[] args) {
+			 
+
+			//TODO call method [getInfo()]
+			getinfo();
+}
