1af3ef3100daaa52fa60c95e6a09cada528300bb
jackietorres <jackietorres@jacquelines-air.fios-router.home>
2022-01-15 00:09:36 -0500

    Broken Code HW

---
+++ src/test/java/Homework/BrokenCode.java
@@ -0,0 +1,68 @@
+package Homework;
+
+import java.util.concurrent.CountDownLatch;
+
+public class BrokenCode {
+
+public static void main(String[] args) { //main was spelled incorrectly
+	
+}		
+		double addResult = addTwoNumbers (2.0,4.0,3.0);
+		
+		System.out.println(addTwoNumbers);	
+
+		
+}		
+				
+		for(int i = 0; i = 10, i++;);{
+			System_out.println("Count= i");
+		}
+		
+		
+		double whosFavNumber(23,1, true);
+		whosFavNumber(3, true);		
+		whosFavNumber(1,);
+				
+		String greeting = myName("John Cena");
+	System.out.println(myName);
+		
+		
+		public static void myName (String name) {
+			System.out.println("My name is " + name);
+		}
+		
+	
+	public void static addTwoNumbers(int numOne int numTwo); {
+
+		int result = numOne + numTwo;
+
+		System.out.println(numOne + " + " + numTwo + " = " + result);
+		
+		return result;
+	}
+	
+	public static String whoseFavNumber(double whoseFavNumber i = '999'){
+		double result = 999;
+		
+		int whoseFavNumber = 999;
+		char number;
+				
+		if ( i = 1) {
+			System.out.println("Brook");
+					
+		} else if (i = 2 || i >  4 && i > 4) ;
+		System.out.println("Dannia");
+			
+		 else   (i > 3) {
+		System.out.println("Helen");
+			
+		}  else 
+			System.out.println("my favorite number");
+		
+		System.out.println(result);
+		
+		}
+		
+	
+	
+
